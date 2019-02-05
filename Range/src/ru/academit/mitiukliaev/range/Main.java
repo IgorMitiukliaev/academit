941b60f45c4de9fc7701088316269efa75ead2e5
Igor Mitiukliaev <igor_m@outlook.com>
2019-02-05 12:26:48 +0700

    На проверку v.1

--- Range/src/ru/academit/mitiukliaev/range/Main.java
+++ Range/src/ru/academit/mitiukliaev/range/Main.java
@@ -2,10 +2,15 @@ package ru.academit.mitiukliaev.range;
 
 public class Main {
     public static void main(String[] args) {
-        Range range_1 = new Range(-5, -1);
-        Range range_2 = new Range(-2, 4);
+        Range range_1 = new Range(-5, 4);
+        Range range_2 = new Range(-3, -2);
 
-        Range[] range_union = new Range[2];
-
+        Range[] range_difference = range_1.getDifference(range_1, range_2);
+        System.out.println(range_difference.length);
+        for (int i=0; i<range_difference.length; i++){
+            System.out.println(i);
+            System.out.println(range_difference[i].getFrom());
+            System.out.println(range_difference[i].getTo());
+        }
     }
 }

aeb7909f9e733e510af38fb3baca0e933cf4eed0
Igor Mitiukliaev <igor_m@outlook.com>
2019-02-18 21:16:14 +0700

    Shapes готов по замечаниям от 14.02 Vector готов пп. 1-3

--- Vector/src/ru/academit/mitiukliaev/Main.java
+++ Vector/src/ru/academit/mitiukliaev/Main.java
@@ -1,4 +1,24 @@
 package ru.academit.mitiukliaev;
 
+import ru.academit.mitiukliaev.vector.*;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Vector vector1 = new Vector(1, 2, 3, 4, 5);
+
+        int n = vector1.getDimension();
+
+        Vector vector2 = new Vector(vector1);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(vector2.getCoordinate(i));
+        }
+        System.out.println(vector2.getDimension());
+
+        double[] a = new double[]{1, 2, 3, 4, 5};
+        Vector vector3 = new Vector(10, a);
+
+        System.out.println(vector3);
+    }
 }
--- Vector/src/ru/academit/mitiukliaev/vector/Vector.java
+++ Vector/src/ru/academit/mitiukliaev/vector/Vector.java
@@ -1,4 +1,58 @@
 package ru.academit.mitiukliaev.vector;
 
 public class Vector {
+    private int dimension;
+    private double[] vector;
+
+    public Vector(int n) {
+        this.dimension = n;
+        vector = new double[this.dimension];
+    }
+
+    public Vector(Vector vector2) {
+        this.dimension = vector2.getDimension();
+        vector = new double[this.dimension];
+        for (int i = 0; i < this.dimension; i++) {
+            vector[i] = vector2.getCoordinate(i);
+        }
+    }
+
+    public Vector(double... coordinates) {
+        this.dimension = coordinates.length;
+        vector = new double[this.dimension];
+        for (int i = 0; i < this.dimension; i++) {
+            vector[i] = coordinates[i];
+        }
+    }
+
+    public Vector(int n, double[] coordinates) {
+        this.dimension = n;
+        this.vector = new double[this.dimension];
+        int coordinatesDimension = coordinates.length;
+        for (int i = 0; i < this.dimension; i++) {
+            if (i < coordinatesDimension) {
+                this.vector[i] = coordinates[i];
+            } else {
+                this.vector[i] = 0;
+            }
+        }
+    }
+
+    public int getDimension() {
+        return this.dimension;
+    }
+
+    public double getCoordinate(int i) {
+        return vector[i];
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        for (int i = 0; i < this.dimension - 1; i++) {
+            sb.append(this.vector[i] + ", ");
+        }
+        sb.append(this.vector[this.dimension - 1] + "}");
+        return sb.toString();
+    }
 }

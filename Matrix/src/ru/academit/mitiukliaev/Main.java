0b13125b1fcf540d4c966192c86dcdc013993b59
Igor Mitiukliaev <igor_m@outlook.com>
2019-03-19 16:05:21 +0700

    Matrix с исправлениями на проверку

--- Matrix/src/ru/academit/mitiukliaev/Main.java
+++ Matrix/src/ru/academit/mitiukliaev/Main.java
@@ -5,18 +5,17 @@ import ru.academit.mitiukliaev.vector.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        Matrix a = new Matrix(10, 10);
-        Matrix b = new Matrix(a);
-
         double[][] array = {{1, 2}, {3, 4}, {5, 6}};
-        Matrix c = new Matrix(array);
-        Vector v1 = new Vector(new double[]{3, 4, 5});
-        Vector v2 = new Vector(new double[]{7, 8, 9, 10});
-        Matrix d = new Matrix(new Vector[]{v1, v2});
-        System.out.println(d.getWidth());
 
-        d.setVector(0, new double[]{1, 1, 1, 1});
-        System.out.println(d.getVector(0));
-        System.out.println(d);
+        Matrix a = new Matrix(new double[][]{{1, 1, 1}, {2, 2, 2}, {3, 3, 3}});
+        Matrix b = new Matrix(new double[][]{{1, 1}, {2, 2}, {3, 3}});
+        Vector v1 = new Vector(new double[]{1, 1, 1});
+
+        System.out.println(a.multiplyVector(v1));
+        System.out.println(Matrix.multiplyMatrices(a, b));
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(v1);
+
     }
 }
--- Matrix/src/ru/academit/mitiukliaev/matrix/Matrix.java
+++ Matrix/src/ru/academit/mitiukliaev/matrix/Matrix.java
@@ -5,122 +5,254 @@ import ru.academit.mitiukliaev.vector.Vector;
 import java.util.Arrays;
 
 public class Matrix {
-    private Vector[] matrix;
+    private Vector[] lines;
 
-    public Matrix(int height, int length) {
-        this.matrix = new Vector[height];
-        for (int i = 0; i < height; i++) {
-            this.matrix[i] = new Vector(length);
+    //Конструкторы не должны позволять создать матрицу размера 0 - DONE
+    public Matrix(int linesCount, int columnsCount) {
+        if (linesCount <= 0 || columnsCount <= 0) {
+            throw new IllegalArgumentException("Size must be positive!");
+        }
+
+        this.lines = new Vector[linesCount];
+        for (int i = 0; i < linesCount; i++) {
+            this.lines[i] = new Vector(columnsCount);
         }
     }
 
+    //Второй конструктор работает неверно, т.к. там в массиве будут те же векторы - DONE
     public Matrix(Matrix source) {
-        this.matrix = Arrays.copyOf(source.matrix, source.getHeight());
-    }
-
-    public Matrix(double[]... source) {
-        int height = source.length;
-        this.matrix = new Vector[height];
-        for (int i = 0; i < height; i++) {
-            this.matrix[i] = new Vector(source[i]);
-        }
-    }
-
-    public Matrix(Vector... source) {
-        int height = source.length;
-        this.matrix = new Vector[height];
-        for (int i = 0; i < height; i++) {
-            this.matrix[i] = new Vector(source[i]);
-        }
-    }
-
-    public int getHeight() {
-        return this.matrix.length;
-    }
-
-    public int getWidth() {
-        int maxWidth = 0;
-        for (Vector e : this.matrix
-        ) {
-            maxWidth = maxWidth > e.getSize() ? maxWidth : e.getSize();
-        }
-        return maxWidth;
-    }
-
-    public void setVector(int index, double... source) {
-        if (!(source.length == this.getWidth())) {
-            throw new IllegalArgumentException("Source size = " + source.length + " / this.size = " + this.getWidth());
-        }
-        if (index < 0 || index >= this.getWidth()) {
-            throw new IndexOutOfBoundsException("Index " + index + " is out of range");
-        }
-        this.matrix[index] = this.matrix[index].multiplyScalar(0).add(new Vector(source));
-        return;
-    }
-
-    public Vector getVector(int index) {
-        if (index < 0 || index >= this.getWidth()) {
-            throw new IndexOutOfBoundsException("Index " + index + " is out of range");
-        }
-        return this.matrix[index];
-    }
-
-
-    public Vector getColumn(int index) {
-        if (index < 0 || index >= this.getHeight()) {
-            throw new IndexOutOfBoundsException("Index " + index + " is out of range");
-        }
-        Vector column = new Vector(this.getHeight());
         int i = 0;
-        for (Vector e : this.matrix
-        ) {
-            column.setCoordinate(i, this.matrix[i].getCoordinate(index));
+        this.lines = new Vector[source.lines.length];
+        for (Vector e : source.lines) {
+            this.lines[i] = new Vector(e);
+            ++i;
+        }
+    }
+
+    //Надо учесть, что в конструкторе от двумерного массива может быть разная длина 2 измерения. - DONE
+    public Matrix(double[]... source) {
+        if (source.length == 0) {
+            throw new IllegalArgumentException("Source is empty");
+        }
+
+        int linesCount = source.length;
+        this.lines = new Vector[linesCount];
+        for (int i = 0; i < linesCount; i++) {
+            this.lines[i] = new Vector(source[i]);
+        }
+        this.setSize(linesCount, this.getColumnsCount());
+    }
+
+    //Надо учесть, что в конструкторе от двумерного массива может быть разная длина 2 измерения. - DONE
+    public Matrix(Vector... source) {
+        if (source.length == 0) {
+            throw new IllegalArgumentException("Source is empty");
+        }
+
+        int linesCount = source.length;
+        this.lines = new Vector[linesCount];
+        for (int i = 0; i < linesCount; i++) {
+            this.lines[i] = new Vector(source[i]);
+        }
+        this.setSize(linesCount, this.getColumnsCount());
+    }
+
+    public void setSize(int linesCount, int columnsCount) {
+        if (linesCount <= 0 || columnsCount <= 0) {
+            throw new IllegalArgumentException("Size must be positive!");
+        }
+
+        int oldLinesCount = this.getLinesCount();
+        this.lines = Arrays.copyOf(this.lines, linesCount);
+
+        for (int i = 0; i < linesCount; i++) {
+            this.lines[i] = (i < oldLinesCount ? this.lines[i].setSize(columnsCount) : new Vector(columnsCount));
+        }
+    }
+
+    public void setElement(int line, int column, double value) {
+        if (line < 0 || line >= this.getLinesCount()) {
+            throw new IllegalArgumentException("Line " + line + " is out of range");
+        }
+        if (column < 0 || line >= this.getColumnsCount()) {
+            throw new IllegalArgumentException("Column " + column + " is out of range");
+        }
+        this.lines[line].setCoordinate(column, value);
+    }
+
+    //Транспонирование матрицы
+    public Matrix transpose() {
+        Matrix tmp = new Matrix(this.getColumnsCount(), this.getLinesCount());
+        for (int i = 0; i < this.getColumnsCount(); i++) {
+            tmp.setLine(i, this.getColumn(i));
+        }
+        return tmp;
+    }
+
+    //Получение размеров матрицы - количество строк
+    public int getLinesCount() {
+        return this.lines.length;
+    }
+
+    //Получение размеров матрицы - количество столбцов
+    // TODO там не нужен цикл
+    public int getColumnsCount() {
+        int columnsCount = 0;
+        for (Vector e : this.lines) {
+            columnsCount = (columnsCount < e.getSize() ? e.getSize() : columnsCount);
+        }
+        return columnsCount;
+    }
+
+    //задание вектора-строки по индексу
+    public void setLine(int index, Vector source) {
+        if (index < 0 || index >= this.getLinesCount()) {
+            throw new IndexOutOfBoundsException("Index " + index + " is out of range");
+        }
+        source.setSize(this.getColumnsCount());
+        this.lines[index] = new Vector(source);
+    }
+
+    //получение вектора-строки по индексу
+    public Vector getLine(int index) {
+        if (index < 0 || index >= this.getLinesCount()) {
+            throw new IndexOutOfBoundsException("Index " + index + " is out of range");
+        }
+        return this.lines[index];
+    }
+
+    //получение вектора-столбца по индексу
+    public Vector getColumn(int index) {
+        if (index < 0 || index >= this.getColumnsCount()) {
+            throw new IndexOutOfBoundsException("Index " + index + " is out of range");
+        }
+        Vector column = new Vector(this.getLinesCount());
+        int i = 0;
+        for (Vector e : this.lines) {
+            column.setCoordinate(i, e.getCoordinate(index));
             ++i;
         }
         return column;
     }
 
-    /*
-            public Matrix transpose() {
-                return this;
-            }
-
-            public Matrix miltiplyScalar(double value) {
-                return this;
-            }
-
-            public Matrix miltiplyVector(Vector vector) {
-                return this;
-            }
-
-            public Matrix add(Matrix matrix) {
-                return this;
-            }
-
-            public Matrix subtract(Matrix matrix) {
-                return this;
-            }
-
-            public double getDeterminant() {
-                return
-            }
-*/
-    @Override
-    public String toString() {
-        return Arrays.toString(this.matrix);
+    //Умножение на скаляр
+    public Matrix multiplyScalar(double value) {
+        int linesCount = this.getLinesCount();
+        for (int i = 0; i < linesCount; i++) {
+            this.lines[i].multiplyScalar(value);
+        }
+        return this;
     }
 
-    /*
-                @Override
-                public boolean equals(Object o) {
+    //умножение матрицы на вектор
+    public Vector multiplyVector(Vector vector) {
+        if (vector.getSize() != this.getColumnsCount()) {
+            String message = "Size(V) = " + vector.getSize();
+            message += "doesn't match to ColumnsCount(M) = ";
+            message += String.valueOf(this.getColumnsCount());
+            throw new IllegalArgumentException(message);
+        }
+        int i = 0;
+        Vector result = new Vector(this.getLinesCount());
+        for (Vector e : this.lines) {
+            result.setCoordinate(i, Vector.productScalar(e, vector));
+            ++i;
+        }
+        return result;
+    }
 
-                }
+    //Сложение матриц
+    public void add(Matrix matrix) {
+        if (matrix.getColumnsCount() != this.getColumnsCount() || matrix.getLinesCount() != this.getLinesCount()) {
+            throw new IllegalArgumentException("Sizes dom't match");
+        }
+        int i = 0;
+        for (Vector e : matrix.lines) {
+            this.lines[i].add(e);
+            ++i;
+        }
+    }
 
-                @Override
-                public int hashCode() {
+    //вычитание матриц
+    public void subtract(Matrix matrix) {
+        if (matrix.getColumnsCount() != this.getColumnsCount() || matrix.getLinesCount() != this.getLinesCount()) {
+            throw new IllegalArgumentException("Sizes dom't match");
+        }
+        int i = 0;
+        for (Vector e : matrix.lines) {
+            this.lines[i].subtract(e);
+            ++i;
+        }
+    }
 
-                }
+    //TODO - Вычисление определителя матрицы. Если не квадратная - бросить исключение
+    // public double getDeterminant() {
+    // return
+    // }
 
-*/
+    public static Matrix addMatrices(Matrix matrix1, Matrix matrix2) {
+        if (matrix1.getColumnsCount() != matrix2.getColumnsCount() || matrix1.getLinesCount() != matrix2.getLinesCount()) {
+            throw new IllegalArgumentException("Sizes dom't match");
+        }
+        Matrix result = new Matrix(matrix1);
+        result.add(matrix2);
+        return result;
+    }
 
+    public static Matrix subtractMatrices(Matrix matrix1, Matrix matrix2) {
+        if (matrix1.getColumnsCount() != matrix2.getColumnsCount() || matrix1.getLinesCount() != matrix2.getLinesCount()) {
+            throw new IllegalArgumentException("Sizes dom't match");
+        }
+        Matrix result = new Matrix(matrix1);
+        result.subtract(matrix2);
+        return result;
+    }
+
+    public static Matrix multiplyMatrices(Matrix matrix1, Matrix matrix2) {
+        if (matrix1.getColumnsCount() != matrix2.getLinesCount()) {
+            throw new IllegalArgumentException("Sizes dom't match");
+        }
+        Matrix result = new Matrix(matrix2.getColumnsCount(), matrix1.getLinesCount());
+        Matrix tmp = matrix2.transpose();
+        int i = 0;
+        for (Vector e : tmp.lines) {
+            result.setLine(i, matrix1.multiplyVector(e));
+            ++i;
+        }
+        result = result.transpose();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        for (Vector e : this.lines) {
+            sb.append(e.toString());
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+        if (o == null || o.getClass() != this.getClass()) {
+            return false;
+        }
+        Matrix matrix = (Matrix) o;
+        return Arrays.equals(this.lines, matrix.lines);
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        for (Vector e : this.lines) {
+            result = result * prime + e.hashCode();
+        }
+        return result;
+    }
 }

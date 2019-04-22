d6092c655da682b8261538f694f87410db4078f0
Igor Mitiukliaev <igor_m@outlook.com>
2019-04-22 17:00:41 +0700

    Лямбды на проверку

--- Lambda/src/ru/academit/mitiukliaev/Main.java
+++ Lambda/src/ru/academit/mitiukliaev/Main.java
@@ -13,37 +13,34 @@ public class Main {
         list.add(new Person("Jane", 27));
         list.add(new Person("Victor", 36));
         list.add(new Person("Victor", 24));
-        list.add(new Person("Victor", 12));
+        list.add(new Person("Victor", 22));
         list.add(new Person("Suzie", 25));
         list.add(new Person("Mary", 32));
-        list.add(new Person("Charles", 14));
-        list.add(new Person("Tiffany", 10));
+        list.add(new Person("Charles", 24));
+        list.add(new Person("Tiffany", 20));
 
         // список уникальных имен
-        String uniqueNamesString = "Names: ";
-        uniqueNamesString += list.stream()
-                .map(x -> x.getName())
+        String uniqueNamesString = list.stream()
+                .map(Person::getName)
                 .distinct()
-                .collect(Collectors.joining(", "));
+                .collect(Collectors.joining(", ", "Names: ", "."));
         System.out.println(uniqueNamesString);
 
         OptionalDouble average = list.stream()
                 .filter(person -> person.getAge() < 18)
-                .mapToDouble(p -> p.getAge())
+                .mapToDouble(Person::getAge)
                 .average();
-        System.out.println(average);
+        System.out.println((average == OptionalDouble.empty() ? "Nobody is under 18" : average));
 
         //получить Map, где ключи - имена, а значения - средний возраст
         Map<String, Double> personsByName = list.stream()
-                .collect(Collectors.groupingBy(p -> p.getName(), Collectors.averagingDouble(p -> p.getAge())));
+                .collect(Collectors.groupingBy(Person::getName, Collectors.averagingDouble(Person::getAge)));
         System.out.println(personsByName);
 
         //получить людей в возрасте от 20 до 45, вывести в консоль их имена в порядке убывания возраста
         list.stream()
-                .filter(p -> p.getAge() < 45 && p.getAge() >= 20)
-                .sorted((p1, p2) -> {
-                    return (int) (p2.getAge() - p1.getAge());
-                })
+                .filter(p -> p.getAge() <= 45 && p.getAge() >= 20)
+                .sorted((p1, p2) -> (int) (p2.getAge() - p1.getAge()))
                 .forEach(p -> System.out.println(p.getName()));
     }
 
